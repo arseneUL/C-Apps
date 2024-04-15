bb0d702e2ea4e6fc9b0cff3c45791f830ceac7f4
arseneUL <146701038+arseneUL@users.noreply.github.com>
2024-04-15 12:16:38 +0200

    Add files via upload

---
+++ LoadingConsoleUI/LoadingConsoleUI/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Timers;
+using System.Threading;
+
+namespace LoadingConsoleUI
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Loading(10,500);
+            Loading(500);
+            Console.ReadLine();
+        }
+
+        /*public static void Loading(int n, int ms)
+        {
+            int count = 0;
+            int count2 = 0;
+            while (count < n)
+            {
+                if (count == 0)
+                {
+                    Console.Write(".");
+                    count++;
+                }
+                else
+                {
+                    Console.Clear();
+                    int seconds = 0;
+                    while (seconds < 1)
+                    {
+                        //Console.WriteLine($"Elapsed seconds: {seconds}");
+                        seconds++;
+                        Thread.Sleep(ms); // Sleep for 1 second (1000 milliseconds)
+                    }
+                    Console.Write(new String ('.',count));
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Boom");
+            Console.ReadLine();
+        }*/
+        public static void Loading(int n, int ms)
+        {
+            int count = 0;
+            int count2 = 0;
+            while (count < n)
+            {
+                if (count == 0)
+                {
+                    Console.Write(".");
+                    count++;
+                }
+                else
+                {
+                    Console.Clear();
+                    while (count2 < count)
+                    {
+                        int seconds = 0;
+                        while (seconds < 1)
+                        {
+                            //Console.WriteLine($"Elapsed seconds: {seconds}");
+                            seconds++;
+                            Thread.Sleep(ms); // Sleep for 1 second (1000 milliseconds)
+                        }
+
+                        
+                        Console.Write(".", count);
+                        count2++;
+                    }
+
+                    count++;
+                    count2 = 0;
+                }
+            }
+            Console.WriteLine( "\n" + "Boom");
+            Console.ReadLine();
+        }
+
+        public static void Loading(int ms)
+        {
+            int count = 0;
+            int count2 = 0;
+            while (true)
+            {
+                if (count == 0)
+                {
+                    Console.Write(".");
+                    count++;
+                }
+                else
+                {
+                    Console.Clear();
+                    while (count2 < 5)
+                    {
+                        int seconds = 0;
+                        while (seconds < 1)
+                        {
+                            //Console.WriteLine($"Elapsed seconds: {seconds}");
+                            seconds++;
+                            Thread.Sleep(ms); // Sleep for 1 second (1000 milliseconds)
+                        }
+
+                        
+                        Console.Write(".", count);
+                        count2++;
+                    }
+                    Thread.Sleep(500);
+                    count++;
+                    count2 = 0;
+                }
+            }
+        }
+    }
+    
+    
+}
